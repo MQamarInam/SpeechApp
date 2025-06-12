a9b7429c2547bda107fd088f09235e3a4a591563
MQamarInam <qamarinam028@gmail.com>
2025-06-12 16:43:36 +0500

    Initial Commit

---
+++ SpeechApp/SpeechAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechAppApp.swift
+//  SpeechApp
+//
+//  Created by Qaim's Macbook  on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeechAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
